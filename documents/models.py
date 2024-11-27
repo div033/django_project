156f44b370fe6edcb8d12fe6f799f28baa9713ec
Divya <divya.prakas@gmail.com>
2024-11-27 11:27:14 +0530

    fixing UUID for propeties in POST document - again

--- documents/models.py
+++ documents/models.py
@@ -21,7 +21,7 @@ class Document(models.Model):
     ]
 
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    property_id = models.UUIDField(null=True, blank=True)
+    property = models.UUIDField(null=True, blank=True)
     title = models.CharField(max_length=255)
     document_type = models.CharField(max_length=20, choices=DOCUMENT_TYPES)
     file = models.FileField(upload_to='property_documents/')
